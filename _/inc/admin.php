1451da797d865d53689baacd04dd428aee1fd509
chris-judge <Chris_Judge941@yahoo.com>
2017-05-15 11:02:27 -0400

    theme clean up, add admin bar back by default

--- _/inc/admin.php
+++ _/inc/admin.php
@@ -6,14 +6,6 @@ function is_login_page() {
     return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
 }
 
-// changing the logo link from wordpress.org to your site
-function quindo_login_url() {  return home_url(); }
-add_filter( 'login_headerurl', 'quindo_login_url' );
-
-// changing the alt text on the logo to show your site name
-function quindo_login_title() { return get_option( 'blogname' ); }
-add_filter( 'login_headertitle', 'quindo_login_title' );
-
 // Remove unwanted items from admin menu
 function quindo_remove_admin_menu_items() {
     $remove_menu_items = array(__('Posts'),__('Comments'),);
@@ -34,14 +26,6 @@ function quindo_editor_styles() {
     add_editor_style( '_/css/editor-style'.$quindo_file_suffix.'.css' );
 }
 add_action( 'after_setup_theme', 'quindo_editor_styles' );
-
-// Register styles for login page
-// function quindo_login_css() {
-//     if (WP_ENV === 'production')
-//         $quindo_file_suffix = '.min';
-//     wp_enqueue_style( 'logincss', get_template_directory_uri() . '/_/css/login'.$quindo_file_suffix.'.css?'.filemtime(get_template_directory()."/_/css/login" . $quindo_file_suffix . ".css"), false );
-// }
-// add_action( 'login_enqueue_scripts', 'quindo_login_css', 10 );
 
 // Allow Thumbnails in Posts
 add_theme_support( 'post-thumbnails' );
